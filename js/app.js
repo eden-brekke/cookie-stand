53d96e15d460c1610a743bc6921e67c0c998ca79
Eden Brekke <eden.lorrai@gmail.com>
2022-02-16 10:08:20 -0800

    changed up times to be X:00a/pm

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // set variables
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 let stores = [];
 let salesTable = document.getElementById('sales table');
 let grandTotal = 0;
